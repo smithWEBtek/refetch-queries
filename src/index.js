cf039ac4328eecb3091e93a5f8b0277d8f959eb0
brad@smithwebtek.com <brad@smithwebtek.com>
2022-03-19 16:12:14 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+  // uri: "https://react-todo-graphql.herokuapp.com/v1/graphql"
+  uri: "https://swt-graphql.herokuapp.com/v1/graphql",
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
